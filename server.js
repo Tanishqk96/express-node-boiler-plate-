8e33906ff77abe822ca5818614c59a13a231bc1c
TANISHQ <korgaonkartanishq@gmail.com>
2025-05-20 09:42:36 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/Contact-routes"))
+app.get('/', (req, res) => res.send('Hello World'));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
